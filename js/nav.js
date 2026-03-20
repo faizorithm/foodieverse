4e8b8c79c4c205f8132d2b0de204bba5d9ddbbe8
Faiza <faizasarker123@gmail.com>
2026-03-20 01:26:00 -0400

    nav.js

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    let menu = document.getElementById("myLinks");
+  
+    if (menu.style.display === "flex") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "flex";
+    }
+  }
+  
